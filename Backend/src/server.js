604cff904397b34f33b41a32e88009f703a9a154
Rahulsaraf <63157871+Rahulsaraf1578@users.noreply.github.com>
2023-01-06 11:49:30 +0530

    Working with url parameters
    In server.js we are learning how we can send data in the server using parameters

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -8,6 +8,13 @@ app.post('/hello',(req,res)=>{
     res.send(`Hello ${req.body.name} `);
 })
 
+app.get('/hello/:name/goodbye/:otherName',(req,res)=>{
+    // req.params will give us acess to all the parameters of the request
+    console.log(req.params)
+    const name = req.params.name;
+    res.send(`Hello ${name}!!`)
+})
+
 app.listen(8000, ()=>{
     console.log('Server is listening to port 8000');
 })
