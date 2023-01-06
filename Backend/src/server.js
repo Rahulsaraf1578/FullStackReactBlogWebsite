703abb21ae9375eccbb4f853ad2d3ea04b4127a9
Rahulsaraf <63157871+Rahulsaraf1578@users.noreply.github.com>
2023-01-06 12:01:52 +0530

    Upvoting article
    We have learnt how to make upvote endpoint using fake data we will make this real with mongodb but for logic point we have done this.

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,18 +1,40 @@
 import express from "express";
 
+let articlesInfo = [{
+    name:'learn-react',
+    upvotes:0,
+},{
+    name:'learn-node',
+    upvotes:0,
+},{
+    name:'learn-mongodb',
+    upvotes:0,
+}]
+
 const app = express();
 app.use(express.json());
 
-app.post('/hello',(req,res)=>{
-    console.log(req.body);
-    res.send(`Hello ${req.body.name} `);
-})
+// app.post('/hello',(req,res)=>{
+//     console.log(req.body);
+//     res.send(`Hello ${req.body.name} `);
+// })
 
-app.get('/hello/:name/goodbye/:otherName',(req,res)=>{
-    // req.params will give us acess to all the parameters of the request
-    console.log(req.params)
-    const name = req.params.name;
-    res.send(`Hello ${name}!!`)
+// app.get('/hello/:name',(req,res)=>{
+//     // req.params will give us acess to all the parameters of the request
+//     const name = req.params.name;
+//     res.send(`Hello ${name}!!`)
+// })
+
+app.put('/api/articles/:name/upvote',(req,res)=>{
+    const {name} = req.params;
+    const article = articlesInfo.find(a => a.name === name);
+    if(article){
+        article.upvotes += 1;
+        res.send(`The ${name} article now has ${article.upvotes} upvotes`)
+    }else{
+        res.send("That article doesnt exist");
+    }
+
 })
 
 app.listen(8000, ()=>{
