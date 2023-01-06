83c6d81a3335c1acc2ea361df740675fa2931b7c
Rahulsaraf <63157871+Rahulsaraf1578@users.noreply.github.com>
2023-01-06 22:15:20 +0530

    Made the comments end point
    Here we have made the end point for comments and we were repeating some of the code to connect to data base instead we made a new function in db.js file and we imported it so we can remove duplicate code.

---
+++ Backend/src/db.js
@@ -0,0 +1,17 @@
+let db;
+import { MongoClient } from "mongodb";
+
+async function connectToDb(cb) {
+    const client = new MongoClient('mongodb://127.0.0.1:27017');
+    
+    // Connecting this client with asynchronus 
+    await client.connect();
+
+    db = client.db('react-blog-db');
+    cb();
+}
+
+export {
+    db,
+    connectToDb,
+}
