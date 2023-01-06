ff883823990203c8502ff4d1a458e0c1063144a7
Rahulsaraf <63157871+Rahulsaraf1578@users.noreply.github.com>
2023-01-06 21:52:37 +0530

    Connected mongodb with express
    We are using async to connect with database and then find our database that we want to connect.
    
    After connecting we are fetching article from the database and then sending it as response.

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,21 +1,29 @@
 import express from "express";
-
-let articlesInfo = [{
-    name:'learn-react',
-    upvotes:0,
-    comments:[],
-},{
-    name:'learn-node',
-    upvotes:0,
-    comments:[],
-},{
-    name:'learn-mongodb',
-    upvotes:0,
-    comments:[],
-}]
+import { MongoClient } from "mongodb";
 
 const app = express();
 app.use(express.json());
+
+// End point for getting articles data
+app.get('/api/articles/:name',async (req,res)=>{
+    const {name} = req.params;
+
+    const client = new MongoClient('mongodb://127.0.0.1:27017');
+    
+    // Connecting this client with asynchronus 
+    await client.connect();
+
+    const db = client.db('react-blog-db');
+
+    const article = await db.collection('articles').findOne({name});
+
+    if(article){
+        res.json(article);
+    }else{
+        res.sendStatus(404);
+    }
+})
+
 
 // app.post('/hello',(req,res)=>{
 //     console.log(req.body);
