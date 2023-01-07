38a3c274b457d37f5ca2208b643459d2b89531b4
Rahulsaraf <63157871+Rahulsaraf1578@users.noreply.github.com>
2023-01-07 10:58:16 +0530

    Adding data loading to page components
    We are importing axios package first.

--- Frontend/src/pages/ArticlePage.js
+++ Frontend/src/pages/ArticlePage.js
@@ -1,32 +1,37 @@
-// Form routing of specific article based on an id
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import articles from "./Article-content";
-import NotFound from "./NotFound";
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import NotFoundPage from './NotFound';
+import articles from './Article-content';
 
-const ArticlePage = () =>{
-    const [ArticleInfo,setArticleInfo] = useState({upvotes:0,comments:[]});
+const ArticlePage = () => {
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+    const { articleId } = useParams();
 
-    useEffect(()=>{
-        setArticleInfo({upvotes:3,commments:[]});
-    });
+    useEffect(() => {
+        const loadArticleInfo = async () => {
+            const response = await axios.get(`/api/articles/${articleId}`);
+            const newArticleInfo = response.data;
+            setArticleInfo(newArticleInfo);
+        }
 
-    const {articleId} = useParams();
-    const article = articles.find(article => article.name ===articleId)
+        loadArticleInfo();
+    }, []);
 
-    if(!article){
-        return <NotFound/>
+    const article = articles.find(article => article.name === articleId);
+
+    if (!article) {
+        return <NotFoundPage />
     }
 
-    return(
+    return (
         <>
-            <h1>{article.title}</h1>
-            <p>This article has {ArticleInfo.upvotes} and upvote(s) </p>
-            {article.content.map(paragraph =>(
-                <p key={paragraph}>{paragraph}</p>
-            ))}
+        <h1>{article.title}</h1>
+        <p>This article has {articleInfo.upvotes} upvote(s)</p>
+        {article.content.map((paragraph, i) => (
+            <p key={i}>{paragraph}</p>
+        ))}
         </>
-        
     );
 }
 
