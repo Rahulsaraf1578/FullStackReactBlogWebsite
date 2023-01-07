c91647b408bac75a54bc63d9965cacf70cd0d8a0
Rahulsaraf <63157871+Rahulsaraf1578@users.noreply.github.com>
2023-01-07 11:16:56 +0530

    Added upvote button
    Here we have added an upvote button and when the button is click add upvote function is run which in backend increases the upvotes and then sends the updated data to us and we are displayiing that updated data.

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -40,7 +40,7 @@ app.put('/api/articles/:name/upvote',async (req,res)=>{
     const article = await db.collection('articles').findOne({name})
     
     if(article){
-        res.send(`The ${name} article now has ${article.upvotes} upvotes`)
+        res.send(article)
     }else{
         res.send("That article doesnt exist");
     }
