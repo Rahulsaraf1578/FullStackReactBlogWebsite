3a9bb9703cda9476565e078a14f3d8876eb0ec27
Rahulsaraf <63157871+Rahulsaraf1578@users.noreply.github.com>
2023-01-07 10:42:42 +0530

    Adding react hooks
    We have to get response for the article from server and then we have to store that response somewhere for that we will use state.
    
    We are using useEffect to load the data from the server.
    
    Then we have made the basic setup of the hooks to set our data.

--- Frontend/src/pages/ArticlePage.js
+++ Frontend/src/pages/ArticlePage.js
@@ -1,9 +1,16 @@
 // Form routing of specific article based on an id
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import articles from "./Article-content";
 import NotFound from "./NotFound";
 
 const ArticlePage = () =>{
+    const [ArticleInfo,setArticleInfo] = useState({upvotes:0,comments:[]});
+
+    useEffect(()=>{
+        setArticleInfo({upvotes:3,commments:[]});
+    });
+
     const {articleId} = useParams();
     const article = articles.find(article => article.name ===articleId)
 
@@ -14,6 +21,7 @@ const ArticlePage = () =>{
     return(
         <>
             <h1>{article.title}</h1>
+            <p>This article has {ArticleInfo.upvotes} and upvote(s) </p>
             {article.content.map(paragraph =>(
                 <p key={paragraph}>{paragraph}</p>
             ))}
