78ff7bbb6ee70808bf44214b347b2fcff233a1c8
Rahulsaraf <63157871+Rahulsaraf1578@users.noreply.github.com>
2023-01-08 10:56:43 +0530

    updating files

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -14,12 +14,34 @@ admin.initializeApp({
 const app = express();
 app.use(express.json());
 
+// Using this middleware when user send it from frontend
+// We are verifying that user with firebase credentials
+// It is like when user sends headers with a api request
+app.use(async (req,res,next)=>{
+    const {authToken} = req.headers;
+
+    if(authToken){
+        try{
+            req.user = await admin.auth().verifyIdToken();
+        }catch(e){
+            res.sendStatus(400);
+        }
+    }
+    next();
+})
+
 app.get('/api/articles/:name', async (req, res) => {
     const { name } = req.params;
+    // Getting user id from firebase
+    const { uid } = req.user;
 
     const article = await db.collection('articles').findOne({ name });
 
     if (article) {
+        const upvoteIds  = article.upvoteIds || [];
+        // If user is not logged in he can't upvote
+        // If he has already upvoted then he can't upvote
+        article.canUpvote = uid && !upvoteIds.include(uid);
         res.json(article);
     } else {
         res.sendStatus(404);
